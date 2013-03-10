0385c315a7b48f02fb957d0b0224720c55022d55
Joshua Oakes <joshuaoakes@hotmail.com>
2013-03-10 17:08:21 +1000

    Moved RequestHandler to separate file and moved interface to BaseRequest

---
+++ HelloAgatha/Agatha.Service/RequestHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace Agatha.Service
+{
+    using Common;
+
+    public abstract class RequestHandler<TRequest, TResponse> : ServiceLayer.RequestHandler<TRequest, TResponse> 
+        where TRequest : Request
+        where TResponse : Response
+    {
+        public override Response Handle(TRequest request)
+        {
+            return Response(request);
+        }
+
+        public abstract TResponse Response(TRequest request);
+    }
+}
